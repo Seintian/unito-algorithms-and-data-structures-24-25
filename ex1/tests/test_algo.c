5679f9b5a48a3bac9c7d5dd0d42bcabba558be60
Seintian <chrstnsntrll@gmail.com>
2024-10-17 13:29:38 +0200

    included Unity files in the Makefile and wrote a basic test to check Unity's working

--- ex1/tests/test_algo.c
+++ ex1/tests/test_algo.c
@@ -2,3 +2,22 @@
 #include "../../Resources/C/Unity/unity.h" // Include Unity for CUnit
 #include <stdio.h>
 
+
+void setUp(void) {
+    // Code to set up before each test (if needed)
+}
+
+void tearDown(void) {
+    // Code to clean up after each test (if needed)
+}
+
+// DEBUG: This is a test to check if the test is working
+void test_example(void) {
+    TEST_ASSERT_EQUAL(1, 1);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_example);
+    return UNITY_END();
+}
