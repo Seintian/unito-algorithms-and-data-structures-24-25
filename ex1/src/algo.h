78a9212c2462eb9c44493b2646e3432c473e8900
Seintian <chrstnsntrll@gmail.com>
2024-10-17 11:58:04 +0200

    added test.c file with Unity headers' path

--- ex1/src/algo.h
+++ ex1/src/algo.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+
 void merge_sort(void **base, size_t nitems, int (*compar)(const void*, const void*));
 void quick_sort(void **base, size_t nitems, int (*compar)(const void*, const void*));
 
---
+++ ex1/tests/test_algo.c
@@ -0,0 +1,4 @@
+#include "../src/algo.h"
+#include "../../Resources/C/Unity/unity.h" // Include Unity for CUnit
+#include <stdio.h>
+
