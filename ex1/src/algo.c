21c3aad3e135880eecd10ee70e28d0331a9a8a32
Seintian <chrstnsntrll@gmail.com>
2024-10-17 11:46:14 +0200

    first ex1 commit: added Makefile and basic `src/` files

---
+++ ex1/src/algo.c
@@ -0,0 +1,10 @@
+#include "algo.h"
+
+
+void merge_sort(void **base, size_t nitems, int (*compar)(const void*, const void*)) {
+
+}
+
+void quick_sort(void **base, size_t nitems, int (*compar)(const void*, const void*)) {
+
+}
---
+++ ex1/src/algo.h
@@ -0,0 +1,9 @@
+#ifndef _ALGO_H
+#define _ALGO_H
+
+#include <stdlib.h>
+
+void merge_sort(void **base, size_t nitems, int (*compar)(const void*, const void*));
+void quick_sort(void **base, size_t nitems, int (*compar)(const void*, const void*));
+
+#endif
