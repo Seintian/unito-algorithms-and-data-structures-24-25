20ec04609795312b341865a5dfd5732406129b9c
“Samuele <samuele.taboga@edu.unito.it>
2024-11-26 15:44:14 +0100

    Add test header for graph module functions

---
+++ ex3-4/tests/test_graph.h
@@ -0,0 +1,20 @@
+#ifndef _TEST_GRAPH_H
+#define _TEST_GRAPH_H
+
+// Test functions for graph module
+void test_graph_create();
+void test_graph_add_node();
+void test_graph_add_edge();
+void test_graph_contains_node();
+void test_graph_contains_edge();
+void test_graph_remove_node();
+void test_graph_remove_edge();
+void test_graph_get_nodes();
+void test_graph_get_edges();
+void test_graph_get_neighbours();
+void test_graph_get_label();
+void test_graph_num_nodes();
+void test_graph_num_edges();
+void test_graph_free();
+
+#endif // _TEST_GRAPH_H
