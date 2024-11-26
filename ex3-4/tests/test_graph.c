bdb9e70f074f2851cc9683ee04b300053e79ef55
“Samuele <samuele.taboga@edu.unito.it>
2024-11-26 15:55:14 +0100

    Added implementations for test_graph_get_neighbours(), test_graph_get_label(), test_graph_num_nodes() functions in test_graph.c

--- ex3-4/tests/test_graph.c
+++ ex3-4/tests/test_graph.c
@@ -128,3 +128,45 @@ void test_graph_get_edges() {
     free(edges);
     graph_free(graph);
 }
+
+void test_graph_get_neighbours() {
+    Graph graph = graph_create(1, 1, compare_ints, hash_ints);
+    int node1 = 1, node2 = 2, label = 100;
+
+    graph_add_node(graph, &node1);
+    graph_add_node(graph, &node2);
+    graph_add_edge(graph, &node1, &node2, &label);
+
+    void** neighbours = graph_get_neighbours(graph, &node1);
+    TEST_ASSERT_NOT_NULL(neighbours);
+    TEST_ASSERT_EQUAL(1, graph_num_neighbours(graph, &node1));
+    TEST_ASSERT_EQUAL(&node2, neighbours[0]);
+
+    free(neighbours);
+    graph_free(graph);
+}
+
+void test_graph_get_label() {
+    Graph graph = graph_create(1, 1, compare_ints, hash_ints);
+    int node1 = 1, node2 = 2, label = 100;
+
+    graph_add_node(graph, &node1);
+    graph_add_node(graph, &node2);
+    graph_add_edge(graph, &node1, &node2, &label);
+
+    TEST_ASSERT_EQUAL(&label, graph_get_label(graph, &node1, &node2));
+
+    graph_free(graph);
+}
+
+void test_graph_num_nodes() {
+    Graph graph = graph_create(0, 0, compare_ints, hash_ints);
+    int node1 = 1, node2 = 2;
+
+    graph_add_node(graph, &node1);
+    graph_add_node(graph, &node2);
+
+    TEST_ASSERT_EQUAL(2, graph_num_nodes(graph));
+
+    graph_free(graph);
+}
